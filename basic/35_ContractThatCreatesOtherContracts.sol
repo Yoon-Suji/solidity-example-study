59de19801a07cf0ce4aaab839a900c21e7db95d2
Yoon-Suji <70956926+Yoon-Suji@users.noreply.github.com>
2022-08-21 18:06:13 +0900

    Create 35_ContractThatCreatesOtherContracts.sol

---
+++ basic/35_ContractThatCreatesOtherContracts.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+/*
+  - 컨트랙트는 new 키워드를 사용해서 다른 컨트랙트에 의해서 생성될 수 있음 (버전 0.8.0 이상부터)
+  - new 키워드는 create2를 지원함 (salt 옵션 참고)
+*/
+
+contract Car {
+    address public owner;
+    string public model;
+    address public carAddr;
+
+    constructor(address _owner, string memory _model) payable {
+        owner = _owner;
+        model = _model;
+        carAddr = address(this);
+    }
+}
+
+contract CarFactory {
+    Car[] public cars;
+
+    function create(address _owner, string memory _model) public {
+        Car car = new Car(_owner, _model);
+        cars.push(car);
+    }
+
+    function createAndSendEther(address _owner, string memory _model) public payable {
+        Car car = (new Car){value: msg.value}(_owner, _model);
+        cars.push(car);
+    }
+
+    function create2(
+        address _owner,
+        string memory _model,
+        bytes32 _salt
+    ) public {
+        Car car = (new Car){salt: _salt}(_owner, _model);
+        cars.push(car);
+    }
+
+    function create2AndSendEther(
+        address _owner,
+        string memory _model,
+        bytes32 _salt
+    ) public payable {
+        Car car = (new Car){value: msg.value, salt: _salt}(_owner, _model);
+        cars.push(car);
+    }
+
+    function getCar(uint _index)
+        public
+        view
+        returns (
+            address owner,
+            string memory model,
+            address carAddr,
+            uint balance
+        )
+    {
+        Car car = cars[_index];
+
+        return (car.owner(), car.model(), car.carAddr(), address(car).balance);
+    }
+}
